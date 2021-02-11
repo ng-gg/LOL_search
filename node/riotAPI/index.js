ae630590efdd4b7ead7db95cb0ee50131f85b9c7
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2021-02-12 02:38:27 +0900

    chore: 주석 제거

--- node/riotAPI/index.js
+++ node/riotAPI/index.js
@@ -7,10 +7,6 @@ module.exports = {
     return new Promise(function (resolve, reject) {
       const encodedName = urlencode(name);
 
-      // const url =
-      //   `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodedName}` +
-      //   `?api_key=${process.env.RIOT_API_KEY}`;
-
       https.get(urls.summoner_v4(encodedName), function (response) {
         response.on('data', function (data) {
           const summonerData = JSON.parse(data);
@@ -22,10 +18,6 @@ module.exports = {
 
   getSummonerMantchHistory: function (accountId, beginIndex, endIndex) {
     return new Promise(function (resolve, reject) {
-      // const url =
-      //   `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}` +
-      //   `?endIndex=${endIndex}&beginIndex=${beginIndex}&api_key=${process.env.RIOT_API_KEY}`;
-
       https.get(
         urls.match_v4_matchList(accountId, beginIndex, endIndex),
         function (response) {
