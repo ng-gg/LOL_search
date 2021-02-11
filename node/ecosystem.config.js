cba4b7e942c3f94bc536f8e28b813ed8ce088535
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2021-02-11 17:51:01 +0900

    feat(PM2): update setting file
    - update ecosystem.config.js
    
    changed all '`' to '''

--- node/ecosystem.config.js
+++ node/ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: ‘app’,
-      script: ‘./app.js’,
+      name: 'app',
+      script: './app.js',
       instances: 0,
-      exec_mode: ‘cluster’,
+      exec_mode: 'cluster',
       wait_ready: true,
       listen_timeout: 5000,
       kill_timeout: 5000,
