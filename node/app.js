017e06e8d1a4879964df166aee6190414d5fff50
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2021-02-12 01:18:33 +0900

    refactor: url 템플릿 리터럴로 변경

--- node/app.js
+++ node/app.js
@@ -16,11 +16,11 @@ app.use(function (req, res, next) {
 function getSummonerInfoByName(name) {
   return new Promise(function (resolve, reject) {
     const encodedName = urlencode(name);
+
     const url =
-      'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/' +
-      encodedName +
-      '?api_key=' +
-      process.env.RIOT_API_KEY;
+      `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodedName}` +
+      `?api_key=${process.env.RIOT_API_KEY}`;
+
     https.get(url, function (response) {
       response.on('data', function (data) {
         const summonerData = JSON.parse(data);
@@ -32,14 +32,9 @@ function getSummonerInfoByName(name) {
 function getSummonerMantchHistory(accountId, beginIndex, endIndex) {
   return new Promise(function (resolve, reject) {
     const url =
-      'https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/' +
-      accountId +
-      '?endIndex=' +
-      endIndex +
-      '&beginIndex=' +
-      beginIndex +
-      '&api_key=' +
-      process.env.RIOT_API_KEY;
+      `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}` +
+      `?endIndex=${endIndex}&beginIndex=${beginIndex}&api_key=${process.env.RIOT_API_KEY}`;
+
     https.get(url, function (response) {
       response.on('data', function (data) {
         const matchHistory = JSON.parse(data);
@@ -48,18 +43,18 @@ function getSummonerMantchHistory(accountId, beginIndex, endIndex) {
     });
   });
 }
+
 app.get('/', async function (req, res) {
   const summonerInfo = await getSummonerInfoByName('그어억');
   res.send(summonerInfo);
-  //res.send(summonerInfo);
-  //res.send(result);
-  //res.send(‘Hello World!‘);
 });
+
 app.get('/:summonerName', async function (req, res) {
   const summonerName = req.params.summonerName;
   const summonerInfo = await getSummonerInfoByName(summonerName);
   res.send(summonerInfo);
 });
+
 app.get('/match/:summonerName', async function (req, res) {
   const summonerName = req.params.summonerName;
   const { accountId } = await getSummonerInfoByName(summonerName);
@@ -72,10 +67,12 @@ app.get('/match/:summonerName', async function (req, res) {
   );
   res.send(matchHistory);
 });
+
 app.listen(port, function () {
   process.send('ready');
-  console.log('application is listening on port ${port}...');
+  console.log(`application is listening on port ${port}...`);
 });
+
 process.on('SIGINT', function () {
   isDisableKeepAlive = true;
   app.close(function () {
