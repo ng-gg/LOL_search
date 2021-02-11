f54bb984f83f781890f35168094d49ab4ef21aaa
leejiwoo2002 <leejiwoo2002@naver.com>
2021-02-11 23:59:25 +0900

    chore : border 옵션 수정
    - 중앙과 우측 section의 border 겹치는 CSS 수정

--- react/src/Components/Section/Middle.jsx
+++ react/src/Components/Section/Middle.jsx
@@ -7,7 +7,9 @@ const Container = styled.div`
   width: 50%;
   height: 100%;
   background: ${color.modalBackgroundGray};
-  border: 1px solid ${color.modalBorder};
+  border-top: 1px solid ${color.modalBorder};
+  border-bottom: 1px solid ${color.modalBorder};
+  border-right: 1px solid ${color.modalBorder};
   backdrop-filter: blur(4px);
   -webkit-backdrop-filter: blur(4px);
   box-sizing: border-box;
