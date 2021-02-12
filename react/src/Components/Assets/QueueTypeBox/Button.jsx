249a7581c1957c133055d5e536bb7efd7f06ece4
leejiwoo2002 <leejiwoo2002@naver.com>
2021-02-12 22:17:04 +0900

    feat : queueType Button CSS 추가
    - hover시 그림자 transition 추가

--- react/src/Components/Assets/QueueTypeBox/Button.jsx
+++ react/src/Components/Assets/QueueTypeBox/Button.jsx
@@ -5,16 +5,18 @@ const Container = styled.button`
   width: 20%;
   height: 100%;
   background: rgba(255, 255, 255, 0.25);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.2);
   backdrop-filter: blur(4px);
   -webkit-backdrop-filter: blur(4px);
   border-radius: 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
   margin-right: 1rem;
+  transition-property: background-color, box-shadow;
+  transition-duration: 0.5s;
 
   :hover {
     pointer: cursor;
     background: rgba(255, 255, 255, 0.6);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.2);
   }
 
   :focus {
--- react/src/Components/Assets/SearchBox/index.jsx
+++ react/src/Components/Assets/SearchBox/index.jsx
@@ -11,7 +11,6 @@ const RowContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   background: rgba(255, 255, 255, 0.25);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.2);
   backdrop-filter: blur(4px);
   -webkit-backdrop-filter: blur(4px);
   border-radius: 10px;
