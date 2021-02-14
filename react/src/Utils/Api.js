3a09d5db91de4efa768447e2ed32cc11713e3a88
leejiwoo2002 <leejiwoo2002@naver.com>
2021-02-14 16:04:32 +0900

    feat : api 관리 코드 생성
    - 유저 정보를 받아오는 api 생성

---
+++ react/src/Utils/Api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const config = {
+  baseURL: 'http://nggg.kro.kr:3000',
+};
+const api = {
+  getProfile: async function (summonerName) {
+    const response = await axios.get(
+      `/api/summoner/profile/${summonerName}`,
+      config
+    );
+    return response;
+  },
+};
+
+export default api;
